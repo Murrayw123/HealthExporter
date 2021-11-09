dff26daf1791140616bceb965134caecb8ef86c4
Murray <murraycwatts@gmail.com>
2021-11-09 20:13:17 +0800

    adding in a scheduler

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import datetime
+
 from database import Database
 from my_fitness_pal_scraper import MyFitnessPalParser
 from repcount_parser import RepcountParser
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 class TaskRunner:
@@ -11,10 +14,14 @@ class TaskRunner:
         self.__mfp_parser = MyFitnessPalParser(write_fn)
 
     def run_task(self):
+        print('running task at', datetime.datetime.utcnow())
         self.__repcount_parser.parse_csv()
         self.__mfp_parser.get_daily_summary()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tr = TaskRunner()
-    tr.run_task()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tr.run_task, "interval", seconds=3600)
+    scheduler.start()
+
