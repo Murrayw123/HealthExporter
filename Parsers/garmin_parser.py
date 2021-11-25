a460ef708be46da58f68a4eb815fd23f3061ea9d
Murray <murraycwatts@gmail.com>
2021-11-25 18:56:29 +0800

    test if the garmin parser is connected

--- Parsers/garmin_parser.py
+++ Parsers/garmin_parser.py
@@ -10,29 +10,32 @@ from dynaconf import settings
 class GarminConnectParser(Parser):
     def __init__(self, db_writer):
         super().__init__(db_writer)
+        self.__connected = False
         try:
             self.__client = Garmin(
                 settings.GARMIN_CONNECT_EMAIL, settings.GARMIN_CONNECT_PASSWORD
             )
             self.__client.login()
+            self.__connected = True
         except Exception as e:
             print("Garmin Connect Error", e)
 
     def parse(self):
-        today = date.today()
+        if self.__connected:
+            today = date.today()
 
-        today_with_hours = datetime(today.year, today.month, today.day, 11, 59)
+            today_with_hours = datetime(today.year, today.month, today.day, 11, 59)
 
-        steps = self.__client.get_steps_data(date.today().isoformat())
-        total_steps = sum(step["steps"] for step in steps)
-        daily_stats = self.__client.get_stats(date.today().isoformat())
-        calories = daily_stats['totalKilocalories']
-        point = (
-            Point("Daily Energy")
-            .field("Steps", total_steps)
-            .field("Calories Burned", calories)
-            .time(
-                time=today_with_hours,
+            steps = self.__client.get_steps_data(date.today().isoformat())
+            total_steps = sum(step["steps"] for step in steps)
+            daily_stats = self.__client.get_stats(date.today().isoformat())
+            calories = daily_stats['totalKilocalories']
+            point = (
+                Point("Daily Energy")
+                .field("Steps", total_steps)
+                .field("Calories Burned", calories)
+                .time(
+                    time=today_with_hours,
+                )
             )
-        )
-        self._db_writer(point)
+            self._db_writer(point)
